d9d35a1375aacd21d74191729220318fbc0fc37e
Anthony Cole <anthony.cole83@yahoo.com>
2023-03-11 13:12:48 -0600

    created both GET routs for server and client.  Made functions to get information from the database and ensured the server is working.  However, the issue is I am getting an error 404

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,6 +4,42 @@ function getReady(){
 console.log('in the getready function');
 
 //create 'addTask' listener
-
+$('#addButton').on('click', addTaskButton);
 //create a 'taskComplete' listener
+getTask();
 };//End getReady Function
+
+//function to get the button to work and get data.
+function addTaskButton(){
+console.log('in the addTaskButton');
+
+
+};
+
+//Create a GET function to get the data from the router
+function getTask(){
+    console.log('In getTask function');
+    $('#viewTasks').empty();//emptys the array
+    $.ajax({
+        type: 'GET',
+        url: '/taskList'
+    }).then(function(response) {
+        console.log('GET /task response', response);
+        //appending data to the DOM
+
+        for( let i=0; i < response.length; i++){
+            $('#viewTasks').append(`
+            <thread>
+            <tr data-id=${response[i].id}>
+                <th>${response[i].task}</th>
+                <th>${response[i].completetask}</th>
+                <th>${response[i].timecompleted}</th>
+                <th>${response[i].notes}</th>
+                <td><button class=deleteButton>Delete Task</button></th>
+            </tr>
+        </thread>
+            
+            `);
+        };
+    });
+};//end GET and append
--- server/routes/tasklist.router.js
+++ server/routes/tasklist.router.js
@@ -1,11 +1,24 @@
-const {response} = require("express");
+
 const express = require('express');
 const tasklistRouter = express.Router();
-const pool = require('..modules/pool.js');
+const pool = require('../modules/pool');
 
 
 //create a GET route
-
+tasklistRouter.get('/taskList', (req, res)=>{
+    console.log('In the taskListRouter GET, trying to get some tasks');
+    const queryText = 'SELECT * FROM "TaskList"';
+    console.log('submitting Query to DB', queryText);
+    
+    pool.query(queryText)
+    .then((result)=>{
+    console.log('All my result info', result);
+    res.send(result.row);
+    }).catch((err)=>{
+        console.log(`Error making query${queryText}`, err)
+        res.sendStatus(500);
+    });  
+});
 
 
 //create a POST route
