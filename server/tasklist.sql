1ea02694e3a7528751ede067a46716f521315b74
Anthony Cole <anthony.cole83@yahoo.com>
2023-03-13 10:11:51 -0500

    updated the tasklist.sql to be accurate

--- server/tasklist.sql
+++ server/tasklist.sql
@@ -1,13 +1,10 @@
 -- this is from my database regarding the table of tasks I meant to complete
-
 CREATE TABLE "TaskList" (
 	"id" serial primary key,
 	"task" varchar(30) not null,
-	"complete task" varchar(1),
-	"time completed" integer,
+	"complete" boolean,
 	"notes" varchar(250)
 	);
 	
-	INSERT INTO "TaskList"("task", "complete task", "time completed", "notes")
-	VALUES('Sort Desk', 'Y', '1120', 'was not too messy.'), ('Do laundry', 'N',  '1', 'Meant to get it done, but otherthings got in the way'),('Take dog for walk', 'Y', '0910', 'happy doggo')
-	
+	INSERT INTO "TaskList"("task", "complete", "notes")
+	VALUES('Sort Desk', true, 'was not too messy'), ('Do laundry', false, 'Meant to get it done, but otherthings got in the way'),('Take dog for walk', true, 'happy doggo')
