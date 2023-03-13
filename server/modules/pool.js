da8b94408b7a8af95ac6935ba95c6d11fa57257b
Anthony Cole <anthony.cole83@yahoo.com>
2023-03-13 17:52:47 -0500

    trying to debug the pool.js file and find out why my heroku isnt working properly.  took out some ',' and some ';' as well as took away a () infront of Pool in line 3.

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,22 +1,21 @@
- const pg = require("pg");
+const pg = require("pg");
+const express = require('express');
+const pool = new pg.Pool;
 
- const pool = new pg.Pool
-
- if(process.env.DATABASE_URL){
-   pool = new pg.Pool({
-      connectionString:process.env.DATABASE_URL,
-      ssl:{
-         rejectUnauthorized: false
-      };
-   });
- }
- else{
-    pool = new pg.Pool({
-      database: "weekend-to-do-app",
-      host: "localhost",
-      port: 5432,
-      idleTimeoutMillis: 30000
- });
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  })
+} else {
+  pool = new pg.Pool({
+    database: "weekend-to-do-app",
+    host: "localhost",
+    port: 5432,
+    idleTimeoutMillis: 30000
+  })
 }
 
- module.exports = pool;
+module.exports = pool;
