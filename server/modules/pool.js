e0888e430ab6297ab2e16cbc5b67b3699f69c9df
Anthony Cole <anthony.cole83@yahoo.com>
2023-03-13 17:33:38 -0500

    Updated the pool.js file to incorporate the heroku information.

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,9 +1,22 @@
  const pg = require("pg");
 
- const pool = new pg.Pool({
-    database: "weekend-to-do-app",
-    host: "localhost",
-    port: 5432
+ const pool = new pg.Pool
+
+ if(process.env.DATABASE_URL){
+   pool = new pg.Pool({
+      connectionString:process.env.DATABASE_URL,
+      ssl:{
+         rejectUnauthorized: false
+      };
+   });
+ }
+ else{
+    pool = new pg.Pool({
+      database: "weekend-to-do-app",
+      host: "localhost",
+      port: 5432,
+      idleTimeoutMillis: 30000
  });
+}
 
  module.exports = pool;
